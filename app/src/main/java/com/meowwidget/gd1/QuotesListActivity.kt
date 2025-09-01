1c16facd60fcf0ac88f49a6c6de64b9006305f0f
glassrose81-ui <glassrose81@gmail.com>
2025-09-01 23:20:25 +0700

    Update QuotesListActivity.kt

--- app/src/main/java/com/meowwidget/gd1/QuotesListActivity.kt
+++ app/src/main/java/com/meowwidget/gd1/QuotesListActivity.kt
@@ -6,9 +6,6 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import android.widget.ArrayAdapter
-import android.widget.Toast
-
 
 class QuotesListActivity : AppCompatActivity() {
     private val PREF = "meow_settings"
@@ -33,27 +30,6 @@ class QuotesListActivity : AppCompatActivity() {
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
         list.adapter = adapter
-        else if (mode == "fav") {
-    val sp = getSharedPreferences(PREF, MODE_PRIVATE)
-    // Lấy toàn bộ quote (mặc định + bạn thêm), lọc những câu đang được đánh dấu yêu thích
-    val all = getAllQuotes(this)
-    val favs = all.filter { sp.getBoolean("fav_" + it, false) }.toMutableList()
-
-    titleView.text = "Yêu thích (${favs.size})"   // đổi tên biến cho khớp với TextView tiêu đề của bạn
-    val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, favs)
-    list.adapter = adapter                         // đổi tên biến cho khớp với ListView của bạn
-
-    // Nhấn giữ để bỏ khỏi Yêu thích
-    list.setOnItemLongClickListener { _, _, position, _ ->
-        val q = favs[position]
-        sp.edit().remove("fav_" + q).apply()
-        favs.removeAt(position)
-        adapter.notifyDataSetChanged()
-        Toast.makeText(this, "Đã bỏ khỏi Yêu thích", Toast.LENGTH_SHORT).show()
-        true
-    }
-}
-
 
         if (mode == "added") {
             list.setOnItemLongClickListener { _, _, position, _ ->
