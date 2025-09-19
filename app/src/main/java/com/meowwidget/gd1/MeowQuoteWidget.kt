8d8dcbb81cf2bd682a562aa9d1a51d0d02c0a4c6
glassrose81-ui <glassrose81@gmail.com>
2025-09-20 00:40:11 +0700

    Update MeowQuoteWidget.kt

--- app/src/main/java/com/meowwidget/gd1/MeowQuoteWidget.kt
+++ app/src/main/java/com/meowwidget/gd1/MeowQuoteWidget.kt
@@ -283,8 +283,8 @@ override fun onEnabled(context: Context) {
             } catch (_: Exception) { 0L }
         }
 
-        var days = daysBetween(anchorDay, todayStr)
-        var steps = days * slotsPerDay + slotIdxToday
+        
+        var steps = ensurePlanBase(sp, baseList.size, now).toLong() * slotsPerDay + slotIdxToday.toLong()
 
         // 0h fix: trước mốc đầu, coi như còn thuộc "hôm qua"
         if (slots.isNotEmpty()) {
